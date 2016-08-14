e029da3023881799c2e1c135f4f0c47ed5413bb5
adolf <adolf666@126.com>
2016-08-14 20:06:57 +0800

    初次提交，刚建项目

---
+++ app/src/main/java/com/adolf/zhouzhuang/LaunchActivity.java
@@ -0,0 +1,13 @@
+package com.adolf.zhouzhuang;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LaunchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
